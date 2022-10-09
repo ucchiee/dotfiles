62c183b83560e783868cd913b0d76269f284a408
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-09 13:15:22 +0900

    install-ndev.sh: renamed and refactored

--- bin/bin/install-ndev.sh
+++ bin/bin/install-ndev.sh
@@ -3,18 +3,20 @@
 case "$(uname -s)" in
   Darwin)
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+    rm -rf nvim-macos
     tar xvzf nvim-macos.tar.gz
-    mv ./nvim-macos/{bin,lib,share} .
+    mv ./nvim-macos ./nvim-dev
     rm nvim-macos.tar.gz
   ;;
   Linux)
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+    rm -rf nvim-linux
     tar xvzf nvim-linux64.tar.gz
-    mv ./nvim-linux/{bin,lib,share} .
+    mv ./nvim-linux ./nvim-dev
     rm nvim-linux64.tar.gz
   ;;
   *) echo "Not supported OS type"
   ;;
 esac
 
-mv ./bin/nvim ./bin/nvim-nighty
+ln -sf ./nvim-dev/bin/nvim ndev
