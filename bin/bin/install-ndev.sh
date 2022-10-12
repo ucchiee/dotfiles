271bdedf2864eef61fe3862108a767c304c72407
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-12 14:06:34 +0900

    install-ndev.sh: fixed

--- bin/bin/install-ndev.sh
+++ bin/bin/install-ndev.sh
@@ -3,14 +3,14 @@
 case "$(uname -s)" in
   Darwin)
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
-    rm -rf nvim-macos
+    rm -rf nvim-macos nvim-dev
     tar xvzf nvim-macos.tar.gz
     mv ./nvim-macos ./nvim-dev
     rm nvim-macos.tar.gz
   ;;
   Linux)
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-    rm -rf nvim-linux
+    rm -rf nvim-linux nvim-dev
     tar xvzf nvim-linux64.tar.gz
     mv ./nvim-linux ./nvim-dev
     rm nvim-linux64.tar.gz
