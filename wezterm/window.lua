c386150ac5811ed8fa6c029a0ecb6335cfff0100
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-06 21:23:16 +0900

    wezterm/window.lua: changed window border

--- wezterm/window.lua
+++ wezterm/window.lua
@@ -5,16 +5,16 @@ function module.apply_to_config(config)
   config.hide_tab_bar_if_only_one_tab = true
 
   -- Window
-  -- border
   config.window_frame = {
+    -- border
     border_left_width = "0.2cell",
     border_right_width = "0.2cell",
     border_bottom_height = "0.1cell",
     border_top_height = "0.1cell",
-    border_left_color = "purple",
-    border_right_color = "purple",
-    border_bottom_color = "purple",
-    border_top_color = "purple",
+    border_left_color = "#625a54",
+    border_right_color = "#625a54",
+    border_bottom_color = "#625a54",
+    border_top_color = "#625a54",
   }
   -- no title bar
   config.window_decorations = "RESIZE"
