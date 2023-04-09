5ddcfc25e5c040c45316e4e325a2f4874a780534
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-09 20:37:29 +0900

    wezterm/general.lua: ssh-related

--- wezterm/general.lua
+++ wezterm/general.lua
@@ -3,13 +3,10 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-  config.color_scheme = "gruvbox-material"
-
-  config.font = wezterm.font_with_fallback({
-    { family = "JetBrainsMono Nerd Font Mono", weight = "DemiBold" },
-    { family = "Hiragino Sans", weight = "DemiBold" },
-  })
-  config.font_size = 14
+	config.ssh_domains = wezterm.default_ssh_domains()
+	for _, dom in ipairs(config.ssh_domains) do
+		dom.assume_shell = "Posix"
+	end
 end
 
 return module
