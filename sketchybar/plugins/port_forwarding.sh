7837c1ba906155585e3e5c343e7256a30ad1230b
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-12 00:10:40 +0900

    sketchybar: added port_forwarding.sh

---
+++ sketchybar/plugins/port_forwarding.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# The $NAME variable is passed from sketchybar and holds the name of
+# the item invoking this script:
+# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+
+SSH_PID=$(ps aux | grep "ssh -fN" | grep -v grep | awk '{ print $2 }')
+
+if [ -n "$SSH_PID" ]; then
+	UCH_MSG="FD-OK"
+else
+	UCH_MSG="FD-NO"
+fi
+
+sketchybar --set $NAME label=$UCH_MSG
