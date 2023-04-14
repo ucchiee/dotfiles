04fe49b5df5f79ea557a1569acdfd49da4ef46fe
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-14 12:15:54 +0900

    wezterm: keybind.lua -> bindings.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,7 +11,7 @@ require("font").apply_to_config(config)
 require("colorscheme").apply_to_config(config)
 require("window").apply_to_config(config)
 require("pane").apply_to_config(config)
-require("keybind").apply_to_config(config)
+require("bindings").apply_to_config(config)
 require("hyperlink").apply_to_config(config)
 
 return config
